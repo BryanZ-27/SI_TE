b7c4ff5fc90a072d4b5902bf5cf2194f910fe84e
Boyang Zheng <75612512+BryanZ-27@users.noreply.github.com>
2024-08-10 14:45:49 +0800

    Update and rename FigS6.R to FigS5.R

--- FigS5.R
+++ FigS5.R
@@ -5,13 +5,13 @@ library(ggplot2)
 
 
 ###load data
-load("./Data_for_FigS6.RData")
+load("./Data_for_FigS5.RData")
 
 
 
 ###main
-#figure S6A
-FIGS6.A <- fig.s6a_data %>% 
+#figure S5A
+FIGS5.A <- fig.s5a_data %>% 
   ggplot()+
   geom_rect(data = chr_rect[seq(1,15,by=2),],mapping = aes(xmin = x1,xmax=x2, fill=c), ymin = -Inf,ymax = Inf,show.legend = F)+
   geom_point(aes(x=Marker_id,y=value,color=variable), size = 0.1)+
@@ -28,8 +28,8 @@ FIGS6.A <- fig.s6a_data %>%
         panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
         legend.text = element_text(size=12,face = "bold"),legend.position = "none")
 
-#figure S6B
-FigureS6.B <- fig.s6b_data %>% 
+#figure S5B
+FigureS5.B <- fig.s5b_data %>% 
   ggplot()+
   geom_point(aes(x=TE_mean_bio,y=SI_mean_bio), size = 0.2)+
   stat_cor(aes(x=TE_mean_bio,y=SI_mean_bio),method = "spearman",cor.coef.name = "rho", size = 8/.pt)+
@@ -43,18 +43,18 @@ FigureS6.B <- fig.s6b_data %>%
                         panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
                         legend.position = "none")
 
-#figure S6C
-FIGS6.C <- fig.s6c_data %>%
+#figure S5C
+FIGS5.C <- fig.s5c_data %>%
   na.omit() %>%
   ggplot(group=!!sym("7905399_chrXII_660371_C_T"))+
   geom_point(aes(x=!!sym("7905399_chrXII_660371_C_T"),y=pheno,color = !!sym("7905399_chrXII_660371_C_T")),position = position_jitter(w = 0.2, h = 0), 
              size = 0.3)+
-  geom_errorbar(aes(ymin=fig.s6c_summ$m[1]-fig.s6c_summ$se[1],ymax=fig.s6c_summ$m[1]+fig.s6c_summ$se[1],x=1),
+  geom_errorbar(aes(ymin=fig.s5c_summ$m[1]-fig.s5c_summ$se[1],ymax=fig.s5c_summ$m[1]+fig.s5c_summ$se[1],x=1),
                 size=0.3,width=0.2)+
-  geom_errorbar(aes(ymin=fig.s6c_summ$m[2]-fig.s6c_summ$se[2],ymax=fig.s6c_summ$m[2]+fig.s6c_summ$se[2],x=2),
+  geom_errorbar(aes(ymin=fig.s5c_summ$m[2]-fig.s5c_summ$se[2],ymax=fig.s5c_summ$m[2]+fig.s5c_summ$se[2],x=2),
                 size=0.3,width=0.2)+
-  geom_segment(aes(y = fig.s6c_summ$m[1], yend = fig.s6c_summ$m[1], x = 0.75, xend = 1.25), size = 0.3)+
-  geom_segment(aes(y = fig.s6c_summ$m[2], yend = fig.s6c_summ$m[2], x = 1.75, xend = 2.25), size = 0.3)+
+  geom_segment(aes(y = fig.s5c_summ$m[1], yend = fig.s5c_summ$m[1], x = 0.75, xend = 1.25), size = 0.3)+
+  geom_segment(aes(y = fig.s5c_summ$m[2], yend = fig.s5c_summ$m[2], x = 1.75, xend = 2.25), size = 0.3)+
   scale_x_discrete(labels= c("BY","RM"))+
   scale_color_manual(values = c("#e5d851", "#8c07bb"))+
   scale_y_continuous(limits=c(1.9, 13.9), breaks = seq(3, 12, 3), labels = seq(3, 12, 3))+
@@ -66,18 +66,18 @@ FIGS6.C <- fig.s6c_data %>%
                         panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
                         legend.position = "none")
 
-#figure S6D
-FIGS6.D <- fig.s6d_data %>%
+#figure S5D
+FIGS5.D <- fig.s5d_data %>%
   na.omit() %>%
   ggplot(group=!!sym("9709121_chrXIV_461485_A_G"))+
   geom_point(aes(x=!!sym("9709121_chrXIV_461485_A_G"),y=pheno,color = !!sym("9709121_chrXIV_461485_A_G")),position = position_jitter(w = 0.2, h = 0), 
              size = 0.3)+
-  geom_errorbar(aes(ymin=fig.s6d_summ$m[1]-fig.s6d_summ$se[1],ymax=fig.s6d_summ$m[1]+fig.s6d_summ$se[1],x=1),
+  geom_errorbar(aes(ymin=fig.s5d_summ$m[1]-fig.s5d_summ$se[1],ymax=fig.s5d_summ$m[1]+fig.s5d_summ$se[1],x=1),
                 size=0.3,width=0.2)+
-  geom_errorbar(aes(ymin=fig.s6d_summ$m[2]-fig.s6d_summ$se[2],ymax=fig.s6d_summ$m[2]+fig.s6d_summ$se[2],x=2),
+  geom_errorbar(aes(ymin=fig.s5d_summ$m[2]-fig.s5d_summ$se[2],ymax=fig.s5d_summ$m[2]+fig.s5d_summ$se[2],x=2),
                 size=0.3,width=0.2)+
-  geom_segment(aes(y = fig.s6d_summ$m[1], yend = fig.s6d_summ$m[1], x = 0.75, xend = 1.25), size = 0.3)+
-  geom_segment(aes(y = fig.s6d_summ$m[2], yend = fig.s6d_summ$m[2], x = 1.75, xend = 2.25), size = 0.3)+
+  geom_segment(aes(y = fig.s5d_summ$m[1], yend = fig.s5d_summ$m[1], x = 0.75, xend = 1.25), size = 0.3)+
+  geom_segment(aes(y = fig.s5d_summ$m[2], yend = fig.s5d_summ$m[2], x = 1.75, xend = 2.25), size = 0.3)+
   scale_x_discrete(labels= c("BY","RM"))+
   scale_color_manual(values = c("#e5d851", "#8c07bb"))+
   scale_y_continuous(limits=c(1.9, 13.9), breaks = seq(3, 12, 3), labels = seq(3, 12, 3))+
@@ -89,8 +89,8 @@ FIGS6.D <- fig.s6d_data %>%
                         panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
                         legend.position = "none")
 
-#figure S6E
-FigureS6.E <- fig.s6e_data %>%
+#figure S5E
+FigureS5.E <- fig.s5e_data %>%
   ggplot()+
   geom_point(aes(x=TE_mean_bio,y=SI_mean_bio), size = 0.2)+
   stat_cor(aes(x=TE_mean_bio,y=SI_mean_bio),method = "spearman",cor.coef.name = "rho", size = 8/.pt)+
@@ -104,8 +104,8 @@ FigureS6.E <- fig.s6e_data %>%
                         panel.grid.major=element_blank(),panel.grid.minor=element_blank(),
                         legend.position = "none")
 
-#figure S6F
-fig.s6f.plot <- fig.s6f_data %>%
+#figure S5F
+fig.s5f.plot <- fig.s5f_data %>%
   ggplot()+
   geom_rect(data = chr_rect[seq(1,15,by=2),],mapping = aes(xmin = x1,xmax=x2, fill=c), ymin = -Inf,ymax = Inf,show.legend = F)+
   geom_point(aes(x=Marker_id,y=value,color=variable), size = 0.1)+
@@ -125,9 +125,9 @@ fig.s6f.plot <- fig.s6f_data %>%
 
 
 ###save
-ggsave(paste("./Fig.S6.A_", Sys.Date(), ".pdf", sep = ""), FIGS6.A, width = 10.8+0.75, height = 6, units = "cm")
-ggsave(paste("./Fig.S6.B_", Sys.Date(), ".pdf", sep = ""), FigureS6.B, width = 3.6-0.5, height = 6-0.75, units = "cm")
-ggsave(paste("./Fig.S6.C_", Sys.Date(), ".pdf", sep = ""), FIGS6.C, width = 3.6, height = 6-1.25, units = "cm")
-ggsave(paste("./Fig.S6.D_", Sys.Date(), ".pdf", sep = ""), FIGS6.D, width = 3.6, height = 6-1.25, units = "cm")
-ggsave(paste("./Fig.S6.E_", Sys.Date(), ".pdf", sep = ""), FigureS6.E, width = 3.6-0.5, height = 6-0.75, units = "cm")
-ggsave(paste("./Fig.S6.F_", Sys.Date(), ".pdf", sep = ""), fig.s6f.plot, width = 10.8+0.75, height = 6, units = "cm")
+ggsave(paste("./Fig.S5.A_", Sys.Date(), ".pdf", sep = ""), FIGS5.A, width = 10.8+0.75, height = 6, units = "cm")
+ggsave(paste("./Fig.S5.B_", Sys.Date(), ".pdf", sep = ""), FigureS5.B, width = 3.6-0.5, height = 6-0.75, units = "cm")
+ggsave(paste("./Fig.S5.C_", Sys.Date(), ".pdf", sep = ""), FIGS5.C, width = 3.6, height = 6-1.25, units = "cm")
+ggsave(paste("./Fig.S5.D_", Sys.Date(), ".pdf", sep = ""), FIGS5.D, width = 3.6, height = 6-1.25, units = "cm")
+ggsave(paste("./Fig.S5.E_", Sys.Date(), ".pdf", sep = ""), FigureS5.E, width = 3.6-0.5, height = 6-0.75, units = "cm")
+ggsave(paste("./Fig.S5.F_", Sys.Date(), ".pdf", sep = ""), fig.s5f.plot, width = 10.8+0.75, height = 6, units = "cm")
